34a5b86cdb0803beee35dfa95fea24eec1e11f0a
SlinkyWalnut <123278624+SlinkyWalnut@users.noreply.github.com>
2023-10-08 01:33:05 +0000

    added guy

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,7 +1,50 @@
-<<<<<<< HEAD
 import random
 with open('/workspaces/ShakeAndBake/Hangman/words.txt', 'r' )as f:
     words = f.readlines()
+
+def print_hangman(wrong):
+    if(wrong == 7):
+        print("\n +--+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 6):
+        print("\n +--+")
+        print(" O  |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 5):
+        print("\n +--+")
+        print(" O  |")
+        print(" |  |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 4):
+        print("\n +--+")
+        print(" O  |")
+        print(" |\ |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 3):
+        print("\n +--+")
+        print(" O  |")
+        print("/|\ |")
+        print("    |")
+        print("   ===")
+    elif(wrong == 2):
+        print("\n +--+")
+        print(" O  |")
+        print("/|\ |")
+        print("/   |")
+        print("   ===")
+    elif(wrong == 1):
+        print("\n +--+")
+        print(" O  |")
+        print("/|\ |")
+        print("/ \ |")
+        print("   ===")
 
 word = random.choice(words)[:-1]
 allowed_errors = 7
@@ -15,7 +58,7 @@ while not done:
         else:
             print("_", end=" ")
     print("")
-
+    print_hangman(allowed_errors)
     guess = input(f"Allowed errors left {allowed_errors}, Next Guess: ")
     guesses.append(guess.lower())
     if guess.lower() not in word.lower():
@@ -32,7 +75,3 @@ if done :
 else:
     print(f"Game over! The word was {word}!")
     
-=======
-import print 
-
->>>>>>> refs/remotes/origin/main
